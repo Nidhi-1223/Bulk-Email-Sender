37ce52df6a2a5289c08b13f527b4bcfc44d5ec80
nidhi1223 <nidhiworah02@gmail.com>
2022-02-20 00:21:46 +0530

    umm idk

--- nidhi.py
+++ nidhi.py
@@ -13,11 +13,11 @@ root.config(bg="powder blue")
 titleFrame = Frame(root)
 titleFrame.grid(row=0, column=0)
 
-# logoImage = PhotoImage(file= '../Bulk_Email_Sender/Assests/self_email_logo.png' )
+logoImage = PhotoImage(file= '../Bulk_Email_Sender/Assests/self_email_logo.png' )
 
-logo_old = Image.open('../Bulk_Email_Sender/Assests/self_email_logo.png')
-logo_resize = logo_old.resize((72, 72))
-logoImage = ImageTk.PhotoImage(logo_resize)
+# logo_old = Image.open('../Bulk_Email_Sender/Assests/self_email_logo.png')
+# logo_resize = logo_old.resize((72, 72))
+# logoImage = ImageTk.PhotoImage(logo_resize)
 
 titleLabel = Label(titleFrame, text= "Email Sender", image= logoImage, compound=LEFT, bg= "pink")
 titleLabel.grid(row=0, column=0)
